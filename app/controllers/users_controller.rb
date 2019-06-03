cc77c022427fc47dcdc1dfb168b35d93424394fe
oksromn <advenua@gmail.com>
2019-06-03 16:03:51 +0300

    fix some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
       }
 
   root to: 'homes#index'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
